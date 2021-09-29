46ff21410579c8be3777e4476cbc3aed6710d76f
KessyRamos <90793682+KessyRamos@users.noreply.github.com>
2021-09-29 17:18:28 -0300

    Primeiro Commit

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace ComparacaoIfElseMes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int mes = 0;
+
+            Console.Write("Digite o mês desejado: ");
+            mes = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("");
+
+            if(mes == 1)
+            {
+                Console.WriteLine("Este mês tem 31 dias");
+            }
+            else if(mes == 2)
+            {
+                Console.WriteLine("Este mês tem 28 dias ou 29 dias nos anos bissextos");
+            }
+            else if(mes == 3)
+            {
+                Console.WriteLine("Este mês tem 31 dias");
+            }
+            else if(mes == 5)
+            {
+                Console.WriteLine("Este mês tem 31 dias");
+            }
+            else if(mes == 7)
+            {
+                Console.WriteLine("Este mês tem 31 dias");
+            }
+            else if(mes == 8)
+            {
+                Console.WriteLine("Este mês tem 31 dias");
+            }
+            else if(mes == 10)
+            {
+                Console.WriteLine("Este mês tem 31 dias");
+            }
+            else if(mes == 12)
+            {
+                Console.WriteLine("Este mês tem 31 dias");
+            }
+             else if(mes == 4)
+            {
+                Console.WriteLine("Este mês tem 30 dias");
+            }
+             else if(mes == 6)
+            {
+                Console.WriteLine("Este mês tem 30 dias");
+            }
+             else if(mes == 9)
+            {
+                Console.WriteLine("Este mês tem 30 dias");
+            }
+             else if(mes == 11)
+            {
+                Console.WriteLine("Este mês tem 30 dias");
+            }
+             Console.WriteLine("");
+
+        }
+    }
+}
